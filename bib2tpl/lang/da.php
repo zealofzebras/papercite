3b08d2001d337c7482aa7addb2b12e690311fb68
petero-dk <2478689+petero-dk@users.noreply.github.com>
2023-04-20 22:28:22 +0200

    translateable templates and bibfilter

---
+++ bib2tpl/lang/da.php
@@ -0,0 +1,32 @@
+<?php
+$translations = array(
+  // groupkey if no grouping is done
+  'all' => 'Alle',
+  // Month names entries are compared against for sorting issues
+  'months' => array('01' => 'januar', '02' => 'februar',
+                    '03' => 'marts', '04' => 'april',
+                    '05' => 'maj', '06' => 'juni',
+                    '07' => 'juli', '08' => 'august',
+                    '09' => 'september', '10' => 'oktober', 
+                    '11' => 'november', '12' => 'december'),
+                    
+  // Representations of entry types used as headlines
+  'entrytypes' => array('article'       => 'Artikler',
+                        'book'          => 'Bøger',
+                        'booklet'       => 'Brochurer',
+                        'conference'    => 'Konferencepapirer',
+                        'inbook'        => 'I Bøger',
+                        'incollection'  => 'I Samlinger',
+                        'inproceedings' => 'I Sagsbehandlinger',
+                        'manual'        => 'Manualer',
+                        'mastersthesis' => 'Kandidatspecialer',
+                        'misc'          => 'Diverse',
+                        'phdthesis'     => 'Afhandlinger',
+                        'proceedings'   => 'Sagsbehandlinger',
+                        'techreport'    => 'Tekniske Rapporter',
+                        'unpublished'  => 'Ikke publiserede',
+
+                        // Map non-standard types to this type
+                        'unknown'       => 'misc')
+);
+?>
